69767e1b1e47f753aa6acea397df20fa05cfda95
Rob Jackson <rob@rjackson.me>
2017-12-12 23:27:41 +0000

    Go: Improve Acronym
    - Reduce dependance on bytes package by appending directly to string
    - Remove unnecessary empty-string check; if empty loop won't iterate

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,24 +1,18 @@
 // Package acronym generates acronyms!
 package acronym
 
-import (
-	"bytes"
-	"strings"
-)
+import "strings"
 
 // Abbreviate a string!
 func Abbreviate(s string) string {
+	var acronym string
+
 	s = strings.Replace(s, "-", " ", -1)
 	words := strings.Fields(s)
 
-	if len(words) == 0 {
-		return ""
-	}
-
-	var buffer bytes.Buffer
 	for _, word := range words {
-		buffer.WriteString(strings.ToUpper(string(word[0])))
+		acronym += strings.ToUpper(string(word[0]))
 	}
 
-	return buffer.String()
+	return acronym
 }
