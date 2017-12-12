fe34c4ee748c69288d0cef32563dfe085fc04990
Rob Jackson <rob@rjackson.me>
2017-12-12 23:23:59 +0000

    Go: Solve Acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,17 +1,24 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package acronym generates acronyms!
 package acronym
 
-func Abbreviate(string) string
+import (
+	"bytes"
+	"strings"
+)
 
-// Abbreviate should have a comment documenting it.
+// Abbreviate a string!
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	s = strings.Replace(s, "-", " ", -1)
+	words := strings.Fields(s)
+
+	if len(words) == 0 {
+		return ""
+	}
+
+	var buffer bytes.Buffer
+	for _, word := range words {
+		buffer.WriteString(strings.ToUpper(string(word[0])))
+	}
+
+	return buffer.String()
 }
