6acb78e4611ad3b8f82f3c6651d461dfdd89e587
Rob Jackson <rob@rjackson.me>
2017-12-13 14:17:34 +0000

    Go: Improve Space Age

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,54 +1,27 @@
 // Package space handles immeasurable voids
 package space
 
-import (
-	"strings"
-)
+import "strings"
 
 // Planet is an astronomical body orbiting a star or stellar remnant that is massive enough to be rounded by its own
 // gravity, is not massive enough to cause thermonuclear fusion, and has cleared its neighbouring region of planetesimals.
 type Planet string
 
+// EarthYear in seconds
+const EarthYear = 365.25 * 24 * 60 * 60
+
 // Seconds in each planet's year
-const (
-	EarthYear   = 31557600
-	MercuryYear = EarthYear * 0.2408467
-	VenusYear   = EarthYear * 0.61519726
-	MarsYear    = EarthYear * 1.8808158
-	JupiterYear = EarthYear * 11.862615
-	SaturnYear  = EarthYear * 29.447498
-	UranusYear  = EarthYear * 84.016846
-	NeptuneYear = EarthYear * 164.79132
-)
+var planetYears = map[string]float64{
+	"earth":   EarthYear,
+	"mercury": EarthYear * 0.2408467,
+	"venus":   EarthYear * 0.61519726,
+	"mars":    EarthYear * 1.8808158,
+	"jupiter": EarthYear * 11.862615,
+	"saturn":  EarthYear * 29.447498,
+	"uranus":  EarthYear * 84.016846,
+	"neptune": EarthYear * 164.79132}
 
 // Age calculates how old something is in a given planet's years
 func Age(age float64, planet Planet) float64 {
-
-	switch strings.ToLower(string(planet)) {
-	case "mercury":
-		age /= MercuryYear
-
-	case "venus":
-		age /= VenusYear
-
-	case "earth":
-		age /= EarthYear
-
-	case "mars":
-		age /= MarsYear
-
-	case "jupiter":
-		age /= JupiterYear
-
-	case "saturn":
-		age /= SaturnYear
-
-	case "uranus":
-		age /= UranusYear
-
-	case "neptune":
-		age /= NeptuneYear
-	}
-
-	return age
+	return age / planetYears[strings.ToLower(string(planet))]
 }
