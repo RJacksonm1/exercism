ab1852afe65aac97ee460ce912a806a17c95556c
Rob Jackson <rob@rjackson.me>
2017-12-13 22:13:09 +0000

    Go: Solve Hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,23 @@
+// Package hamming calculates the hamming distance between DNA strands
 package hamming
 
+import "errors"
+
+// Distance returns the hamming distance between DMA strands
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("Cannot compute hamming distance from strands of differing length")
+	}
+
+	var hammingDistance int
+
+	for i := range a {
+		if a[i] == b[i] {
+			continue
+		}
+
+		hammingDistance++
+	}
+
+	return hammingDistance, nil
 }
