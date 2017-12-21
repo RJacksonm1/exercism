de78140d76da6501c15cf56ae1a109c6f72848de
Rob Jackson <rob@rjackson.me>
2017-12-21 14:28:13 +0000

    Go: Solve Tree Building

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -3,6 +3,7 @@ package tree
 import (
 	"errors"
 	"fmt"
+	"sort"
 )
 
 type Record struct {
@@ -20,88 +21,57 @@ func (m Mismatch) Error() string {
 	return "c"
 }
 
+// Build a tree
+// We'll take a two-pass approach for efficiency. The first pass will put together a map
+// of record-IDs to empty nodes, and the second will populate those nodes
 func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
-	root := &Node{}
-	todo := []*Node{root}
-	n := 1
-	for {
-		if len(todo) == 0 {
-			break
-		}
-		newTodo := []*Node(nil)
-		for _, c := range todo {
-			for _, r := range records {
-				if r.Parent == c.ID {
-					if r.ID < c.ID {
-						return nil, errors.New("a")
-					} else if r.ID == c.ID {
-						if r.ID != 0 {
-							return nil, fmt.Errorf("b")
-						}
-					} else {
-						n++
-						switch len(c.Children) {
-						case 0:
-							nn := &Node{ID: r.ID}
-							c.Children = []*Node{nn}
-							newTodo = append(newTodo, nn)
-						case 1:
-							nn := &Node{ID: r.ID}
-							if c.Children[0].ID < r.ID {
-								c.Children = []*Node{c.Children[0], nn}
-								newTodo = append(newTodo, nn)
-							} else {
-								c.Children = []*Node{nn, c.Children[0]}
-								newTodo = append(newTodo, nn)
-							}
-						default:
-							nn := &Node{ID: r.ID}
-							newTodo = append(newTodo, nn)
-						breakpoint:
-							for range []bool{false} {
-								for i, cc := range c.Children {
-									if cc.ID > r.ID {
-										a := make([]*Node, len(c.Children)+1)
-										copy(a, c.Children[:i])
-										copy(a[i+1:], c.Children[i:])
-										copy(a[i:i+1], []*Node{nn})
-										c.Children = a
-										break breakpoint
-									}
-								}
-								c.Children = append(c.Children, nn)
-							}
-						}
-					}
-				}
-			}
-		}
-		todo = newTodo
-	}
-	if n != len(records) {
-		return nil, Mismatch{}
-	}
-	if err := chk(root, len(records)); err != nil {
-		return nil, err
-	}
-	return root, nil
-}
 
-func chk(n *Node, m int) (err error) {
-	if n.ID > m {
-		return fmt.Errorf("z")
-	} else if n.ID == m {
-		return fmt.Errorf("y")
-	} else {
-		for i := 0; i < len(n.Children); i++ {
-			err = chk(n.Children[i], m)
-			if err != nil {
-				return
-			}
+	// Sort the records by ID: We're going through them sequentially
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	nodeMap := make(map[int]*Node, len(records))
+
+	// Build a map of empty nodes and verify records are continuous
+	for i, r := range records {
+		if i != r.ID {
+			return nil, fmt.Errorf("Missing record %d", i)
 		}
-		return
+
+		nodeMap[r.ID] = &Node{r.ID, nil}
 	}
+
+	// Rearrange the nodes into a tree
+	for _, r := range records {
+
+		if r.ID == 0 {
+			if r.Parent != 0 {
+				return nil, errors.New("Root node must not have a parent")
+			}
+
+			continue
+		}
+
+		if r.ID == r.Parent {
+			return nil, fmt.Errorf("Record %d cannot be its own parent", r.ID)
+		}
+
+		if r.ID < r.Parent {
+			return nil, fmt.Errorf("Record %d must have a parent with a smaller ID", r.ID)
+		}
+
+		parentNode, ok := nodeMap[r.Parent]
+		if !ok {
+			return nil, errors.New("No record for parent")
+		}
+
+		parentNode.Children = append(parentNode.Children, nodeMap[r.ID])
+	}
+
+	// And then serve the root
+	return nodeMap[0], nil
 }
