ae5b89f49c4e0495185538c50c9a28a25c32b314
Rob Jackson <rob@rjackson.me>
2018-12-29 22:21:24 +0000

    Erlang: Solve Isogram

--- erlang/isogram/src/isogram.erl
+++ erlang/isogram/src/isogram.erl
@@ -2,5 +2,19 @@
 
 -export([is_isogram/1]).
 
+is_isogram(Phrase) -> is_isogram(string:lowercase(Phrase), []).
 
-is_isogram(_Phrase) -> undefined.
+%% Dashes and spaces are allowed to repeat
+is_isogram([First | Phrase], SeenChars) when [First] == "-"; [First] == " " ->
+    is_isogram(Phrase, [First | SeenChars]);
+
+%% Otherwise, if we've seen a character before return false
+is_isogram([First | Phrase], SeenChars) ->
+    case lists:member(First, SeenChars) of
+        true -> false;
+        false -> is_isogram(Phrase, [First | SeenChars])
+    end;
+
+%% And if we get through the entire phrase without returning false, we have an isogram!
+is_isogram([], _SeenChars) ->
+    true.
