78a8f9ccadb9942ac2703a7a8d4ef25832e62cbe
Rob Jackson <rob@rjackson.me>
2018-12-29 21:34:05 +0000

    Erlang: Solve Grains

--- erlang/grains/src/grains.erl
+++ erlang/grains/src/grains.erl
@@ -2,7 +2,16 @@
 
 -export([square/1, total/0]).
 
+%% How many grains are on Square N on a chessboard, where each square
+%% contains double the previous square's amount of grains.
+%% We could compute this recursively, but this happens to be power of
+%% X series. We _could_ use bitshifting here, but let's keep it simple
+%% for future devs to grok and use math:pow instead
 
-square(_Square) -> undefined.
+square(N) when N < 1; N > 64 ->
+    {error, "square must be between 1 and 64"};
+square(N) -> trunc(math:pow(2, N - 1)).
 
-total() -> undefined.
+%% As each tiles doubles the previous, the sum of all previous values
+%% would be 1 less than the next (65th) theoretical tile
+total() -> trunc(math:pow(2, 64)) - 1.
