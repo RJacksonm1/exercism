0ab811d628ce7d93c1a5b2b85cf1953fbdb2f7f4
Rob Jackson <rob@rjackson.me>
2018-12-29 20:28:12 +0000

    Erlang: Improve Triangle
    - Use pattern matching instead of guards for identifying triangles
    - Group triangle inequality guards together

--- erlang/triangle/src/triangle.erl
+++ erlang/triangle/src/triangle.erl
@@ -5,19 +5,13 @@
 kind(A, B, C) when A =< 0 orelse B =< 0 orelse C =< 0 ->
     {error, "all side lengths must be positive"};
   
-kind(A, B, C) when A + B =< C ->
+kind(A, B, C) when A + B =< C; A + C =< B; B + C =< A ->
     {error, "side lengths violate triangle inequality"};
-  
-kind(A, B, C) when A + C =< B ->
-    {error, "side lengths violate triangle inequality"};
-  
-kind(A, B, C) when B + C =< A ->
-    {error, "side lengths violate triangle inequality"};
-  
-kind(A, B, C) when A == B andalso B == C -> equilateral;
-kind(A, B, _C) when A == B -> isosceles;
-kind(A, _B, C) when A == C -> isosceles;
-kind(_A, B, C) when B == C -> isosceles;
-kind(_A, _B, _C) -> scalene.
+
+kind(A, A, A) -> equilateral;
+kind(A, A, _) -> isosceles;
+kind(A, _, A) -> isosceles;
+kind(_, A, A) -> isosceles;
+kind(_, _, _) -> scalene.
 
 test_version() -> 1.
