2fde76d7c0641a4941a3535b7ea1e4796a67f49f
Rob Jackson <rob@rjackson.me>
2018-12-29 04:58:45 +0000

    Erlang: Add Difference of squares

---
+++ erlang/difference-of-squares/src/difference_of_squares.erl
@@ -0,0 +1,14 @@
+-module(difference_of_squares).
+
+-export([sum_of_squares/1, square_of_sum/1, difference_of_squares/1, test_version/0]).
+
+sum_of_squares(_N) ->
+  undefined.
+
+square_of_sum(_N) ->
+  undefined.
+
+difference_of_squares(_N) ->
+  undefined.
+
+test_version() -> 1.
