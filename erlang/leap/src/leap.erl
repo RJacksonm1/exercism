ee9bff83c0c0f2dc618048f6d0ebed4927c6f88c
Rob Jackson <rob@rjackson.me>
2018-12-29 04:52:43 +0000

    Erlang: Improve Leap
    Does this make the conditions clearer?

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -2,13 +2,10 @@
 
 -export([leap_year/1]).
 
+leap_year(Year) when Year rem 400 == 0 ->
+    true;
 
-leap_year(Year) when
-    Year rem 4 == 0 andalso
-    (
-        Year rem 100 /= 0 orelse
-        Year rem 400 == 0
-    ) ->
+leap_year(Year) when Year rem 4 == 0 andalso Year rem 100 /= 0 ->
     true;
 
 leap_year(_Year) -> 
