c6e9265bcf60aa44e2f00a369b3cbaaa108087c6
Rob Jackson <rob@rjackson.me>
2018-12-29 05:16:53 +0000

    Erlang: Solve Difference of squares

--- erlang/difference-of-squares/src/difference_of_squares.erl
+++ erlang/difference-of-squares/src/difference_of_squares.erl
@@ -2,13 +2,26 @@
 
 -export([sum_of_squares/1, square_of_sum/1, difference_of_squares/1, test_version/0]).
 
-sum_of_squares(_N) ->
-  undefined.
+sum_of_squares(N) ->
+  sum_of_squares(0, N, 0).
 
-square_of_sum(_N) ->
-  undefined.
+sum_of_squares(A, N, Running_total) when A =< N ->
+  sum_of_squares(A + 1, N, Running_total + (A*A));
 
-difference_of_squares(_N) ->
-  undefined.
+sum_of_squares(_, _, Running_total) ->
+  Running_total.
+
+square_of_sum(N) ->
+  Sum = sum_of_naturals(0, N, 0),
+  Sum * Sum.
+
+sum_of_naturals(A, N, Running_total) when A =< N ->
+  sum_of_naturals(A + 1, N, Running_total + A);
+
+sum_of_naturals(_, _, Running_total) ->
+  Running_total.
+
+difference_of_squares(N) ->
+  square_of_sum(N) - sum_of_squares(N).
 
 test_version() -> 1.
