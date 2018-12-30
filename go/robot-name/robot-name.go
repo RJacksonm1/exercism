d428f3b0553f91037630e4a8236b044578e479fa
Rob Jackson <rob@rjackson.me>
2018-12-30 19:32:39 +0000

    Go: Improve Robot Name
    - Store used names as a map to reduce lookup cost
    - Loop instead of recursing to find an unused name

--- go/robot-name/robot-name.go
+++ go/robot-name/robot-name.go
@@ -1,11 +1,19 @@
 package robotname
 
-import "math/rand"
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 const nameLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-var assignedNames []string
+var assignedNames map[string]bool
+
+// Is there a better way of instantiating assignedNames?
+func init() {
+	assignedNames = make(map[string]bool)
+}
 
 // Robot beep boops
 type Robot struct {
@@ -13,58 +21,32 @@ type Robot struct {
 }
 
 // Name gives the roboto a name
-func (r *Robot) Name() (string, error) {
-	if r.name == "" {
-		r.name = GenerateUniqueName()
-		assignedNames = append(assignedNames, r.name)
+func (r *Robot) Name() (name string, err error) {
+	if r.name != "" {
+		return r.name, err
 	}
 
-	return r.name, nil
+	// Let's have 1000 attempts at trying find an unused name
+	// If we're past this, we've used enough names that trying to find the remaining unique combinations is prohibitively expensive
+	for i := 0; i < 1000; i++ {
+		name := fmt.Sprintf("%c%c%3d", nameLetters[rand.Intn(len(nameLetters))], nameLetters[rand.Intn(len(nameLetters))], rand.Intn(1000))
+
+		// Find an unused name
+		if _, previouslyUsed := assignedNames[name]; !previouslyUsed {
+			r.name = name
+			assignedNames[name] = true
+
+			return name, nil
+		}
+	}
+
+	return "", errors.New("All names have (probably) been allocated")
 }
 
 // Reset clears the robot's name
 func (r *Robot) Reset() {
+	// If we were to allow name recycling we'd need to consume this bool in the r.Name() check
+	assignedNames[r.name] = false
 
-	// The collision test doesn't allow recycling names 😕
-	// defer ReleaseName(r.name)
 	r.name = ""
-}
-
-// GenerateUniqueName generates an unassigned name for the robot
-func GenerateUniqueName() string {
-	name := GenerateName()
-
-	// If there's a collision, recurse!
-	// This doesn't handle the scenario where all robot names have been assigned 😨
-	for _, n := range assignedNames {
-		if name == n {
-			name = GenerateUniqueName()
-			break
-		}
-	}
-
-	return name
-}
-
-// GenerateName generates a name for the robot
-func GenerateName() string {
-	return fmt.Sprintf("%c%c%3d",
-		nameLetters[rand.Intn(len(nameLetters))],
-		nameLetters[rand.Intn(len(nameLetters))],
-		rand.Intn(1000))
-}
-
-// ReleaseName releases the name!
-func ReleaseName(name string) {
-	var filteredNamed []string
-
-	for _, n := range assignedNames {
-		if n == name {
-			continue
-		}
-
-		filteredNamed = append(filteredNamed, n)
-	}
-
-	assignedNames = filteredNamed
 }
