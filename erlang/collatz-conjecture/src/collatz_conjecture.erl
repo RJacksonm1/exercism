8dab0cfb08eda851f128e7b62db6704778c5ede2
Rob Jackson <rob@rjackson.me>
2019-01-04 22:20:58 +0000

    Erlang: Solve Collatz Conjecture

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,5 +2,14 @@
 
 -export([steps/1]).
 
+steps(N) -> steps(N, 0).
 
-steps(_N) -> undefined.
+steps(N, _) when N =< 0 ->
+    {error, "Only positive numbers are allowed"};
+steps(1, Iterations) -> Iterations;
+steps(N, Iterations) when N rem 2 == 0 ->
+    steps(round(N / 2), Iterations + 1);
+steps(N, Iterations) when N rem 2 == 1 ->
+    steps(3 * N + 1, Iterations + 1);
+steps(N, Iterations) ->
+    io:format("wtf ~w ~w ~n", [N, Iterations]).
