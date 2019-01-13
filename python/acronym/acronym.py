e0589d9b9fde677a762570ba0a3a53c031eb930e
Rob Jackson <rob@rjackson.me>
2019-01-13 13:43:30 +0000

    Python: Improve Acronym
    - Use a generator instead of a list comprehension

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,2 @@
 def abbreviate(words):
-    return "".join([word[0].upper() for word in words.replace('-', ' ').split()])
+    return "".join(word[0].upper() for word in words.replace('-', ' ').split())
